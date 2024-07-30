a9cda4cc7a4f67451802d4e51fb1114d5f62e563
Robbie Oleynick <rpoleynick@gmail.com>
2024-07-30 14:40:34 -0400

    Draw ball on the screen

--- 05-ponggame-working/Ball.js
+++ 05-ponggame-working/Ball.js
@@ -1,4 +1,23 @@
 class Ball {
+    constructor(x, y, vx, vy, r, c) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.r = r;
+        this.c = c;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.c;
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        ctx.fill();
+        ctx.stroke();
+    }
 
     /**
      * Causes the ball to bounce off of objects on the screen
--- 05-ponggame-working/pong.js
+++ 05-ponggame-working/pong.js
@@ -1,7 +1,7 @@
 const gameboard = document.getElementById("gameboard");
 const cpucheck = document.getElementById("cpucheck");
 const ctx = gameboard.getContext("2d");
-
+const STATE = {STARTUP: 0, PLAYING: 1, GAMEOVER: 2};
 
 let boardWidth = 500;
 let boardHeight = 500;
@@ -17,17 +17,50 @@ let paddleL;
 let paddleR;
 let scoreL = 0;
 let scoreR = 0;
+let state = STATE.STARTUP;
 
 function resetGame() {
-    
+    state = STATE.STARTUP;
+
+    nextTick();
 }
 
 function nextTick() {
-    
+    switch (state) {
+        case STATE.STARTUP:
+            state = startup();
+            break;
+        case STATE.PLAYING:
+            state = playing();
+            break;
+        case STATE.GAMEOVER:
+            state = gameover();
+            break;
+        default:
+            state = STATE.STARTUP;
+            break;
+    }
 }
 
-function play() {
-    
+function startup() {
+    ball = new Ball(boardWidth/2, boardHeight/2, 1, -1, ballRadius, "hotpink");
+    return STATE.PLAYING;
+}
+
+function playing() {
+    draw();
+    return STATE.PLAYING;
+}
+
+function gameover() {
+
+    return STATE.GAMEOVER;
+}
+
+function draw() {
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, boardWidth, boardHeight);
+    ball.draw(ctx);
 }
 
 function updateScore() {
