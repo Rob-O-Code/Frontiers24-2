1c790349076e213ad0ccc746ad5f67022202eb05
Robbie Oleynick <rpoleynick@gmail.com>
2024-08-01 10:11:38 -0400

    Add kite weather comparison example

--- 07-weatherapp-starter/example.js
+++ 07-weatherapp-starter/example.js
@@ -20,7 +20,7 @@
 //                   Get weather from location
 
 function load1() {
-    owmGeocode.city = "Worcester, MA"; // or load from a text box, etc.
+    owmGeocode.city = "Gloucester, MA"; // or load from a text box, etc.
     owmGeocode.request(load2); // link to second function
 }
 
@@ -30,11 +30,46 @@ function load2() {
     owmWeather.request(load3); // link to third function
 }
 
+let firstJSON = {};
 function load3() {
-    const loc = document.getElementById("location");
-    loc.innerHTML = `Running example.js...`;
+    firstJSON = owmWeather.json;
+    owmGeocode.city = "Rockport, MA"; // or load from a text box, etc.
+    owmGeocode.request(load4);
+}
+
+function load4() {
+    owmWeather.lat = owmGeocode.getLat();
+    owmWeather.lon = owmGeocode.getLon();
+    owmWeather.request(load5);
+}
+
+let secondJSON = {};
+function load5() {
+    secondJSON = owmWeather.json;
 
     const weatherReport = document.getElementById("weather-report");
-    // See display.js for more examples
-    weatherReport.innerHTML = `Weather report for ${owmGeocode.city}: ${owmWeather.json.weather[0].description}`;
+    if (Math.abs(firstJSON.wind.gust - secondJSON.wind.gust) < 0.5) {
+        weatherReport.innerHTML = "The gust speeds are about the same";
+    } else if (firstJSON.wind.gust < secondJSON.wind.gust) {
+        weatherReport.innerHTML = "It's better to fly a kite in Rockport";
+    } else {
+        weatherReport.innerHTML = "It's better to fly a kite in Gloucester";
+    }
 }
+
+// function load3() {
+//     const loc = document.getElementById("location");
+//     loc.innerHTML = `Running example.js...`;
+
+//     const weatherReport = document.getElementById("weather-report");
+//     // See display.js for more examples
+//     weatherReport.innerHTML = `Weather report for ${owmGeocode.city}: ${owmWeather.json.weather[0].description}`;
+
+//     if (owmWeather.json.main.temp_max > 80) {
+//         document.body.style.backgroundColor = "red";
+//     } else if (owmWeather.json.main.temp_max > 70) {
+//         document.body.style.backgroundColor = "green";
+//     } else {
+//         document.body.style.backgroundColor = "blue";
+//     }
+// }
